dfb2710c26080dffa2b07d369221ccf669d4cb56
xhafermuja <xhafermuja1233@gmail.com>
2022-08-01 16:37:24 +0200

    installedTEST

--- functions.php
+++ functions.php
@@ -1,3 +1,11 @@
 <?php
 
 // Enqueuing
+
+    function dynamic_menu_enqueue() {
+
+        wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
+
+    }
+
+    add_action( 'wp_enqueue_scripts', 'dynamic_menu_enqueue');
