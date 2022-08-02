d4d06c76236b6fd01556145e92062de3381da5b6
bleronamaxhuni <bleronamaxhuni68@gmail.com>
2022-08-02 14:22:35 +0200

    Header

--- header.php
+++ header.php
@@ -7,77 +7,40 @@
     <title>Document</title>
     <link href="/dist/output.css" rel="stylesheet">
     <?php wp_head(); ?>
-    <?php wp_head(); ?>
 </head>
 <body>
+  <header>
+    <div class="header">
+      <div class="header-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>./icons/Project-logo.png"> 
+      </div>
+        <!-- Dynamic menu -->
+        <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
 
-<div class="flex font-sans">
-  <div class="flex-none w-48 relative">
-    <img src="/classic-utility-jacket.jpg" alt="" class="absolute inset-0 w-full h-full object-cover" loading="lazy" />
-  </div>
-  <form class="flex-auto p-6">
-    <div class="flex flex-wrap">
-      <h1 class="flex-auto text-lg font-semibold text-slate-900">
-        Classic Utility Jacket
-      </h1>
-      <div class="text-lg font-semibold text-slate-500">
-        $110.00
+        <!--Search input-->
+        <div class="search-input">
+        <input type="search" name="" id="search">
+        <img class="img-search" src="<?php echo get_template_directory_uri(); ?>./icons/search.png">
+        </div>
+
+        <div class="register-login-btn">
+          <button class="loginBtn"><a href="#" class="login">Log In</a></button>
+          <button class="registerBtn"><a href="#" class="register">Register</a></button>
+        </div>
+    </div>
+  </header>
+
+  <section class="banner">
+    <div class="first-banner">
+      <div class="search-talents">
+        <h2 class="search-title">Search for best <i>services</i></h2>
+        <div class="search-box">
+          <input type="text" class="input-search" placeholder="Search for new talents...">
+          <button class="searchBtn">Search</button>
+        </div>
       </div>
-      <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-        In stock
+      <div class="person-img">
+      <img class="" src="<?php echo get_template_directory_uri(); ?>./icons/First-banner.png">
       </div>
     </div>
-    <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-      <div class="space-x-2 flex text-sm">
-        <label>
-          <input class="sr-only peer" name="size" type="radio" value="xs" checked />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            XS
-          </div>
-        </label>
-        <label>
-          <input class="sr-only peer" name="size" type="radio" value="s" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            S
-          </div>
-        </label>
-        <label>
-          <input class="sr-only peer" name="size" type="radio" value="m" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            M
-          </div>
-        </label>
-        <label>
-          <input class="sr-only peer" name="size" type="radio" value="l" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            L
-          </div>
-        </label>
-        <label>
-          <input class="sr-only peer" name="size" type="radio" value="xl" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            XL
-          </div>
-        </label>
-      </div>
-    </div>
-    <div class="flex space-x-4 mb-6 text-sm font-medium">
-      <div class="flex-auto flex space-x-4">
-        <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-          Buy now
-        </button>
-        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
-          Add to bag
-        </button>
-      </div>
-      <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
-        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-        </svg>
-      </button>
-    </div>
-    <p class="text-sm text-slate-700">
-      Free shipping on all continental US orders.
-    </p>
-  </form>
-</div>
+  </section>
