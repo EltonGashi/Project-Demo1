d11f0c0b5a43e469e1391d7c25aa1dce79643458
Elton Gashi <93680319+EltonGashi@users.noreply.github.com>
2022-08-02 12:07:48 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Enqueuing
+    function dynamic_menu_enqueue() {
+
+        wp_enqueue_style('customstyle', get_template_directory_uri() . '/dist/output.css', array(), '1.0.0', 'all');
+
+    }
+
+    add_action( 'wp_enqueue_scripts', 'dynamic_menu_enqueue');
