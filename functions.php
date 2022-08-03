6ca8775dd125a2f2bd6b80b20a22a7f2993449be
xhafermuja <xhafermuja1233@gmail.com>
2022-08-03 11:32:17 +0200

    a

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 
         wp_enqueue_style('customstyle', get_template_directory_uri() . '/dist/output.css', array(), '1.0.0', 'all');
 
+
     }
 
     add_action( 'wp_enqueue_scripts', 'dynamic_menu_enqueue');
--- section_one_platform/one_platform.php
+++ section_one_platform/one_platform.php
@@ -5,7 +5,7 @@
     </div>
     <div class="services grid-rows-4">
         <button>
-            <div class="services-card flex">
+            <div class="services-card flex bg-red-500">
                 <div class="services-card-img">
                 <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/seo.png" style="height:80px;">
                 </div>
@@ -126,3 +126,10 @@
         </button>
     </div>
 </section>
+
+
+
+<a href="#" class="block p-6 max-w-sm bg-red rounded-lg border border-gray-200 shadow-md hover:bg-red-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+</a>
