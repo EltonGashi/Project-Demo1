661492fbe0008a473de2d658f753419c821ed782
Enis Gjini <gjinienis148@gmail.com>
2022-08-03 22:17:30 +0200

    Adding path to menu.css inside functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,19 @@
 <?php
 
 // Enqueuing
-    function dynamic_menu_enqueue() {
+function dynamic_menu_enqueue()
+{
 
-        wp_enqueue_style('customstyle', get_template_directory_uri() . '/dist/output.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/dist/output.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('menustyle', get_template_directory_uri() . '/css/menu.css', array(), '1.0.0', 'all');
+}
 
-    }
+add_action('wp_enqueue_scripts', 'dynamic_menu_enqueue');
 
-    add_action( 'wp_enqueue_scripts', 'dynamic_menu_enqueue');
+
+function menu_setup()
+{
+    add_theme_support('menus');
+    register_nav_menu('primary', 'Primary Header Navigation');
+}
+add_action('init', 'menu_setup');
