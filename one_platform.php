8caf05fce46fe26acb807d60ef5fe0cef63555fc
xhafermuja <xhafermuja1233@gmail.com>
2022-08-03 16:59:32 +0200

    latest

--- one_platform.php
+++ one_platform.php
@@ -2,7 +2,7 @@
   <!-- ... -->
 
 
-<section class="bg-bg p-32 w-full">
+<section class="bg-bg p-32">
     <div class="headings block text-center justify-self-center py-12">
         <div class="txt-1">
             <h1 class="text-7xl ">One Platform</h1>
@@ -12,11 +12,11 @@
             <small class="text-7xl text-txtGreen">Solutions</small>
         </div>
     </div>
-    <div class="services grid grid-cols-4 gap-24 ">
-        <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left">
+    <div id ="services" class="grid grid-cols-4 gap-24">
+        <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300 ">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Marketing.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Marketing.png" >
                 </div>
                 <div class="services-card-solution">
                     <div class="txt ">
@@ -29,9 +29,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Pen.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Pen.png" >
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -43,10 +43,10 @@
                 </div>
             </div>
         </button>
-        <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left">
+        <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300 ">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Research.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Research.png" >
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -59,9 +59,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div cid="services-card" class="flex text-left">
+            <div cid="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Marketingx2.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Marketingx2.png" >
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -74,9 +74,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Museum.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Museum.png" style="height:50px;">
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -89,9 +89,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/HandShaking.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/HandShaking.png" style="height:50px;">
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -104,9 +104,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left" >
+            <div id="services-card" class="flex text-left items-center" >
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Headset.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Headset.png" style="height:50px;">
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
@@ -119,9 +119,9 @@
             </div>
         </button>
         <button class="bg-white p-6 rounded-lg hover:bg-txtGreen hover:text-white shadow shadow-black-900 transition duration-300">
-            <div id="services-card" class="flex text-left ">
+            <div id="services-card" class="flex text-left items-center">
                 <div id="services-card-img " class="mx-5">
-                <img src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Statistic.png" style="height:50px;">
+                <img class="object-scale-down h-14" src="<?php echo get_template_directory_uri(); ?>./section_one_platform/images/Statistic.png" style="height:50px;">
                 </div>
                 <div class="services-card-solution">
                     <div class="txt">
