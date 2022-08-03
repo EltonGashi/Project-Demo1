c7805aba02225123f1e9fa905cd7781b43459c5f
EltonGashi <eg52279@ubt-uni.net>
2022-08-03 13:28:03 +0200

    dizajn

--- services-card-users/user-cards.php
+++ services-card-users/user-cards.php
@@ -20,26 +20,26 @@
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6 p-5">
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -48,26 +48,26 @@
  
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -75,26 +75,26 @@
   </div>
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -102,26 +102,26 @@
   </div>
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -131,26 +131,26 @@
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6 p-5">
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -159,26 +159,26 @@
  
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -186,26 +186,26 @@
   </div>
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
@@ -213,26 +213,26 @@
   </div>
   <div class="rounded-lg  max-w-sm">
     <a href="#!">
-      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/main.png" alt=""/>
+      <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/main.png" alt=""/>
     </a>
     <div class="p-6 bg-white">
     <div class="flex items-center gap-6 mt-1">
-    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/ellipse1.png">
+    <img class="w-10 h-10" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/ellipse1.png">
     <h4 class="text-#000000-900  font-medium mb-2 ">Melisa Rosa</h4>
     </div>
       <p class="text-#000000-700 text-base  font-medium mb-4 mt-2">
         I will create custom Wordpress forms
       </p>
       <div class="flex items-center gap-1">
-      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Star.png">
+      <img class="w-5 h-5" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Star.png">
       <p class="">5.0</p>
       </div>
       <hr class="mt-4">
       <div class="flex items-center justify-between mt-4">
-      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/Heart.png">
+      <img class="w-5 h-5 mt-4" src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/Heart.png">
       <button type="button" class="text-white bg-green-700 hover:bg-green-800  rounded-full text-xs px-4 py-2 text-center flex items-center ">
       Like and start talk
-      <img src="<?php echo get_template_directory_uri(); ?>./services-user-cards/img/like.png">
+      <img src="<?php echo get_template_directory_uri(); ?>./services-card-users/img/like.png">
       </button>
        </div>
     </div>
