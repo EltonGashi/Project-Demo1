5223abf0f8a4f1276b1bbc7b6d32d9eae3b1d133
xhafermuja <xhafermuja1233@gmail.com>
2022-08-05 17:37:29 +0200

    #

---
+++ many_solutions/section-textwithimage.php
@@ -0,0 +1,48 @@
+<?php 
+$title = get_sub_field('title');
+$content = get_sub_field('content');
+$image = get_sub_field('image');
+$picture = $image['sizes']['large'];
+$side = get_sub_field('image_side');
+?>
+
+
+<div class="flex flex-row">
+                            
+                            <?php if($side == 'left'): ?>
+
+                                <div class="grid grid-cols-2">
+
+
+                                    <img src="<?php echo $picture;?>">
+
+                                </div>
+                                <div class="grid grid-cols-2">
+
+                                    <h4><?php echo $title;?></h4>
+
+                                    <?php echo $content;?>
+                                    
+                                </div>
+
+                            <?php else:?>
+
+                                <div class="grid grid-cols-2">
+
+                                <h4><?php echo $title;?></h4>
+
+                                    <?php echo $content;?>
+
+
+                                    
+
+                                </div>
+                                <div class="grid grid-cols-2">
+                                <img src="<?php echo $picture;?>">
+                                    
+                                    
+                                </div>
+                            
+                            <?php endif;?>
+
+                        </div>
