10fe100992cff95588ae632a1b3a8a08a50640f9
EltonGashi <eg52279@ubt-uni.net>
2022-08-08 11:22:02 +0200

    #

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} /
 module.exports = {
-  content: ['./*.php', './inc/**/*.php'],
+  content: ['./.php', './inc//*.php'],
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        'txtGreen': '#799F28',
+        'bg': '#F0F0F0'
+      }
+    },
   },
   plugins: [],
 }
