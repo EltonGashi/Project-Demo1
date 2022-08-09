28f10fd03de015bb169c9b24d1e43e81ef56ea42
xhafermuja <xhafermuja1233@gmail.com>
2022-08-09 14:40:30 +0200

    #

--- components/login.php
+++ components/login.php
@@ -1,20 +1,9 @@
-<section id="login" class=" p-20 ">
-    <div class="login rounded-xl flex border-2 border-black">
-        <div class="left-side w-2/4 h-96 rounded-xl items-center bg-customGreen">
-            <div class="top">
-                Login
-            </div>
-            <div class="mid">
-                <form action="#">
-                    <label for="email">Email:</label>
-                    <input type="text">
-                    <label for="password">Password:</label>
-                    <input type="password">
-                </form>
-            </div>
+<section id="login" class="h-screen">
+    <div class="login border-1 border-black">
+        <div class="left-side">
+            
         </div>
-        <div class="right-side bg-customGreen w-2/4 h-96 rounded-xl">
-            2
-        </div>
+
     </div>
+    
 </section>
