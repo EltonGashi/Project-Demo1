0a5e8031468afc57b0e26b558455c8c4c8b447de
EltonGashi <eg52279@ubt-uni.net>
2022-08-09 10:45:43 +0200

    #

--- services-card-users/user-cards.php
+++ services-card-users/user-cards.php
@@ -7,8 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Services</title>
 
-
-  <script src="https://cdn-tailwindcss.vercel.app/"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 </head>
