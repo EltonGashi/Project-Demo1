042a153625a4fd89c332ce51821b747a35a8dce3
xhafermuja <xhafermuja1233@gmail.com>
2022-08-15 17:00:30 +0200

    #Folder Managing , Header img wont recognize !!

--- footer.php
+++ footer.php
@@ -4,45 +4,34 @@
 </div>
 <!-- ////////////// CONTAINER-END ////////////// -->
 
-<footer class=" flex  justify-evenly items-center  inset-x-0  bg-black h-48 text-white font-sans">
+<footer class="bg-black ">
+    <div class="container p-20 text-white grid gap-24 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  sm:gap-4 text-left sm:justify-items-center">
 
-    <div class="footer-icon ">
-        <img class="w-42 h-10  object-fit" src="<?php echo get_template_directory_uri(); ?>./components/HeaderImages/Repeat Grid 8.png">
-    </div>
+            <div class="footer-icon ">
+                <img class="w-42 h-10  object-fit" src="<?php echo get_template_directory_uri(); ?>./assets/img/HeaderImages/Repeat Grid 8.png">
+            </div>
 
-   <div class="footer-address">
-    <ul>
-        <li>
-        <a href="#" class="hover:underline">123 MArket ST.#22B</a>
-        </li>
-        <li>
-        <a href="#" class="hover:underline">Charlttesville, California</a>
-        </li>
-        <li>
-        <a href="#" class="hover:underline">454635</a>
-        </li>
-    </ul>
-   </div>
+            <div class="footer-address ">
+                <ul>
+                    <li>
+                    <a href="#" class="hover:underline">123 MArket ST.#22B</a>
+                    </li>
+                    <li>
+                    <a href="#" class="hover:underline">Charlttesville, California</a>
+                    </li>
+                    <li>
+                    <a href="#" class="hover:underline">454635</a>
+                    </li>
+                </ul>
+            </div>
 
-   <div class="footer-navbar">
-        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-   </div>
-   
-    <div class="footer-socials">
-        <ul>
-            <li>
-            <a href="#" class="hover:underline">Facebook</a>
-            </li>
-            <li>
-            <a href="#" class="hover:underline">Twitter</a>
-            </li>
-            <li>
-            <a href="#" class="hover:underline">Linkedin</a>
-            </li>
-            <li>
-            <a href="#" class="hover:underline">Instagram</a>
-            </li>
-        </ul>
+            <div class="footer-navbar ">
+                <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+            </div>
+        
+            <div class="footer-socials ">
+            <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+            </div>
     </div>
 
 </footer>
--- pages/page-home.php
+++ pages/page-home.php
@@ -1,2 +1,13 @@
-banner 
-postet
+
+<?php 
+/*
+    Template Name:  Home
+*/
+get_header(); ?>
+
+
+    <?php get_template_part('/components1/home/home');?>
+
+
+<?php get_footer(); ?>
+
