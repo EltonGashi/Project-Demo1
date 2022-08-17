c26bd0ced7d66fa3451a2e4257092f07b56f8862
xhafermuja <xhafermuja1233@gmail.com>
2022-08-17 12:58:15 +0200

    #Components Included

--- components/home/categories/section-categories.php
+++ components/home/categories/section-categories.php
@@ -1,11 +1,14 @@
 
-    <h1 class="font-medium flex justify-center text-3xl ">Your work can be done in over 700 different categories</h1>
+   
+<div class="div ">
+   
+   <h1 class="font-medium flex justify-center text-3xl ">Your work can be done in over 700 different categories</h1>
     
     
     <br>
     <br>
     
-    <div class=" flex justify-evenly w-14 h-14 ">
+    <div class=" flex justify-evenly h-14 w-full">
       
     <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/sql-server.png" alt="" />
     <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/wordpress.png" alt="" />
@@ -15,24 +18,22 @@
     <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/domain-registration.png" alt="" />
     <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/java-script.png" alt="" /> 
     
- </div>
-
-
-    <br>
-    <br>
-    <br>
-
-
-   <div class="flex justify-evenly w-14 h-14">
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/monitor(1).png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/android.png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/apple.png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/location.png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/loupe.png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/shopping-cart.png" alt="" />
-    <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/smartphone-call.png" alt="" /> 
    </div>
+    <br>
+    <br>
+    <br>
 
 
+         <div class="flex justify-evenly h-14 w-full">
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/monitor(1).png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/android.png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/apple.png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/location.png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/loupe.png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/shopping-cart.png" alt="" />
+         <img class="" src="<?php echo get_template_directory_uri(); ?>./assets/img/CategoriesImages/smartphone-call.png" alt="" /> 
+         </div>
+
+</div>
 
 
