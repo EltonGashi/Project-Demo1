23f64494ce2a47800cde792604acd742da251c31
EltonGashi <eg52279@ubt-uni.net>
2022-08-26 10:37:40 +0200

    ##

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
 <!-- ////////////// CONTAINER-END ////////////// -->
 
-<footer class="bg-black ">
+<footer class=" bg-black ">
     <div class="container p-20 text-white xl:text-left grid gap-24 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  sm:gap-8 text-left  ">
 
             <div class="footer-icon ">
@@ -29,12 +29,28 @@
                 <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
             </div>
         
-            <div class="footer-socials font-basic text-xl">
+
+           <!----- duhet me provu prap ket pjes
+            <?php  if(have_rows('links')):?>
+                <?php while( have_rows('links')): the_row();
+                
+                $link = get_sub_field('links');
+                
+                ?>
+                 <div class="footer-socials font-basic ">
+                <a href="<?php echo $link['url']; ?>"></a>
+                </div>
+                <?php endwhile;?>
+                <?php endif;?>
+                ----->
+
+            <div class="footer-socials font-basic ">
                 <div class="facebook"><a href="https://www.facebook.com">Facebook</a></div>
                 <div class="twitter"><a href="https://www.twitter.com">Twitter</a></div>
                 <div class="linkedin"><a href="https://www.linkedin.com">LinkedIn</a></div>
                 <div class="instagram"><a href="https://www.instagram.com">Instagram</a></div>
             </div>
+            
     </div>
 
 </footer>
