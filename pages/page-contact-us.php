9a61b99b39219d847194f8852d74569bfdf28914
xhafermuja <xhafermuja1233@gmail.com>
2022-09-05 14:45:54 +0200

    #

--- pages/page-contact-us.php
+++ pages/page-contact-us.php
@@ -10,7 +10,11 @@ get_header();?>
     
         while( have_posts()): the_post(); ?>
             <div class="container bg-bg p-20">
+                <div class="map flex w-full items-center justify-center">
+                    <iframe class="p-5 w-full mb-32" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.1340602021473!2d21.153690515685188!3d42.65851442419338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549f265f06fd05%3A0x63d7962d6ccc4547!2sStarLabs!5e0!3m2!1sen!2s!4v1662374696422!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     
+                </div>
+                <hr>
                     
                 
                 <section class="contact font-sans flex  h-screen">
@@ -25,8 +29,8 @@ get_header();?>
                         <img class=" w-full h-2/4 object-scale-down rounded-3xl" src="<?php echo get_template_directory_uri(); ?>/assets/img/ContactImages/contact-us.png" />
                         </div>
                     </div>
-                    
                 </section>
+                
             </div>
         <?php endwhile;
 
