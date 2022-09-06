5316abc45e32617552b7a4394ee517cd583fc29f
xhafermuja <xhafermuja1233@gmail.com>
2022-09-06 15:43:20 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,8 +6,8 @@
                 <h1 class="text-5xl">Sorry, page not found!</h1>
             </div>
             <div class="section-container">
-                <div class="img">
-                <img class="object-scale-down h-2/4 w-2/4" src="<?php echo get_template_directory_uri(); ?>/assets/img/NotFoundImages/nf.png" />
+                <div class="img ">
+                <img class="mx-auto object-scale-down h-2/4 w-2/4" src="<?php echo get_template_directory_uri(); ?>/assets/img/NotFoundImages/nf.png" />
                 </div>
             </div>
         </section>
